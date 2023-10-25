2b66199a304ca042304a4bd3a0af4330e850bc10
Eduardo Castro <eduardo.castro@g.globo>
2023-10-25 19:48:03 -0300

    chore: move backend folder to apps folder

---
+++ apps/backend/src/routes/map/map.route.ts
@@ -0,0 +1,16 @@
+import { PrismaClient } from '@prisma/client'
+import { Request, Response, Router } from 'express'
+
+const mapRoutes = Router()
+const prisma = new PrismaClient()
+
+mapRoutes.get('/maps', async (_, response: Response) => {
+  try {
+    const maps = await prisma.map.findMany()
+    return response.status(200).json(maps)
+  } catch(error) {
+    console.error(error)
+  }
+})
+
+export { mapRoutes }

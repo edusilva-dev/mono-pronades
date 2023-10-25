78890c09d81ed7accab3c78159f5b0a23cedc145
Eduardo Castro <eduardo.castro@g.globo>
2023-10-25 19:55:59 -0300

    feat: create Map entity

---
+++ packages/rules/src/entities/map/model/index.ts
@@ -0,0 +1,19 @@
+import { Entity } from "../../../shared/entity";
+import { Slug } from "../../../shared/slug";
+
+export interface MapProps {
+  id?: string
+  name: string
+  slug: string
+}
+
+export class Map extends Entity<MapProps> {
+  readonly name: string
+  readonly slug: Slug
+
+  constructor(props: MapProps) {
+    super(props)
+    this.name = props.name
+    this.slug = new Slug(props.slug)
+  }
+}
---
+++ packages/rules/src/shared/slug.ts
@@ -0,0 +1,7 @@
+export class Slug {
+  constructor(slug: string) {
+    if (slug.indexOf('')) {
+      throw new Error("O slug não pode conter espaços")
+    }
+  }
+}

5b5f5149639ecaa6f025655e62c03c27831a718a
Eduardo Castro <eduardo.castro@g.globo>
2023-10-26 16:24:28 -0300

    feat: add rule MapRepository

--- packages/rules/src/entities/map/index.ts
+++ packages/rules/src/entities/map/index.ts
@@ -1,2 +1,4 @@
 export type { MapProps } from './model/map'
 export { Map } from './model/map'
+export { MapRepository } from './provider/map-repository'
+export * from './service'
--- packages/rules/src/entities/map/provider/map-repository.ts
+++ packages/rules/src/entities/map/provider/map-repository.ts
@@ -1,8 +1,8 @@
-import { Map } from "../model/map";
+import { Map } from '../model/map'
 
 export interface MapRepository {
   getAllMaps(): Promise<Map[]>
-  getMap(mapId: string): Promise<Map>
+  getMap(mapId: string): Promise<Map | null>
   createMap(map: Map): Promise<Map>
   updateMap(mapId: string, map: Partial<Map>): Promise<Map>
   deleteMap(mapId: string): Promise<void>

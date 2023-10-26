02b839e1c860ad201fc4df81d8393203a5f73939
Eduardo Castro <eduardo.castro@g.globo>
2023-10-25 23:23:09 -0300

    feaat: create MapRepository interface

---
+++ packages/rules/src/entities/map/provider/map-repository.ts
@@ -0,0 +1,9 @@
+import { Map } from "../model/map";
+
+export interface MapRepository {
+  getAllMaps(): Promise<Map[]>
+  getMap(mapId: string): Promise<Map>
+  createMap(map: Map): Promise<Map>
+  updateMap(mapId: string, map: Partial<Map>): Promise<Map>
+  deleteMap(mapId: string): Promise<void>
+}

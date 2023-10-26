50b41e901508993512d248e2d3fd80114e50bcab
Eduardo Castro <eduardo.castro@g.globo>
2023-10-25 23:23:31 -0300

    feat: create CreateMap use case

---
+++ packages/rules/src/entities/map/service/create-map.ts
@@ -0,0 +1,11 @@
+import { UseCase } from "../../../shared/use-case";
+import { Map } from '../model/map'
+import { MapRepository } from "../provider/map-repository";
+
+export class CreateMap implements UseCase<Map, Map> {
+  constructor(private readonly repository: MapRepository) {}
+
+  async execute(map: Map): Promise<Map> {
+    return await this.repository.createMap(map)
+  }
+}
